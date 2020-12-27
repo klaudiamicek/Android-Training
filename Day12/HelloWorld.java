3069b0310ac318eb68a09df71877a4a028dba6a7
klaudiamicek <67850464+klaudiamicek@users.noreply.github.com>
2020-12-27 18:02:01 -0500

    Create HelloWorld.java

---
+++ Day12/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.foundations;
+//in IntelliJ IDEA CE
+public class HelloWorld {
+    public static void main(String[] args) {
+        //prints "Hello New World!", then new line
+         System.out.println("Hello New World!");
+    }
+}
