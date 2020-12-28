a581f7f1d7464d25628e65fbad0d714a4f24b0ff
klaudiamicek <67850464+klaudiamicek@users.noreply.github.com>
2020-12-27 22:55:20 -0500

    Create Comments.java

---
+++ Day12/Comments.java
@@ -0,0 +1,19 @@
+package com.example.comments;
+
+public class Comments {
+
+    /**
+     * This is the main method of the class
+     * @param args This is just a variable
+     */
+    public static void main(String[] args) {
+
+        int firstNumber = 11; //first number entered
+        int secondNumber = 12; //second number entered
+
+        double average = (firstNumber + secondNumber) / 2.0;
+        /*
+        Hello World
+         */
+    }
+}
